4bfe3d160aa1551d3bee222736fd3d72bcfa63b4
Laurent B <lol.b@free.fr>
2017-09-17 15:35:25 +0200

    Adding test to check that header value is correct

--- test.js
+++ test.js
@@ -40,3 +40,36 @@ test("reply.send automatically add x-response-time header", (t) => {
     });
   });
 });
+
+test("reply.send add x-response-time header representing duration", (t) => {
+  t.plan(8);
+
+  const data = {hello: "world"};
+
+  const fastify = fastifyModule();
+  fastify.register(fastifyRequestTime, (err) => {
+    t.error(err);
+  });
+
+  fastify.get("/", (request, reply) => {
+    setTimeout(() => reply.send(data), 1000);
+  });
+
+  fastify.listen(0, (err) => {
+    t.error(err);
+
+    request({
+      method: "GET",
+      uri: `http://localhost:${fastify.server.address().port}`
+    }, (err, response, body) => {
+      t.error(err);
+      t.strictEqual(response.statusCode, 200);
+      t.strictEqual(response.headers["content-length"], `${body.length}`);
+      t.ok(response.headers["x-response-time"]);
+      t.ok(1000 < parseFloat(response.headers["x-response-time"]));
+      t.deepEqual(JSON.parse(body), data);
+      t.end();
+      fastify.close();
+    });
+  });
+});
