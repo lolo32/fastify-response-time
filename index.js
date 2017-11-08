5475ce4d2b9d54764ab972bd64dd6fcbd37d05bd
Laurent B <lol.b@free.fr>
2017-11-08 14:07:18 +0100

    Correction on onSend hook declaration problem
    At first, the hook was declared inside the onRequest hook, so leads to a problem. Now, the onSend hook is declared outside, and value is passed by property of req object

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fastifyPlugin = require("fastify-plugin");
 
+const symbolRequestTime = Symbol("RequestTimer");
+
 /**
  * Decorators
  *
@@ -14,22 +16,24 @@ module.exports = fastifyPlugin((instance, opts, next) => {
   // Hook to be triggered on request (start time)
   instance.addHook('onRequest', (req, res, next) => {
     // Store the start timer in nanoseconds resolution
-    const requestTime = process.hrtime();
-
-    // Hook to be triggered just before response to be send
-    instance.addHook('onSend', (request, reply, payload, next) => {
-      // Calculate the duration, in nanoseconds …
-      const hrDuration = process.hrtime(requestTime);
-      // … convert it to milliseconds …
-      const duration = Math.round(hrDuration[0]*1e5 + hrDuration[1]/1e4) / 100;
-      // … add the header to the response
-      reply.header("X-Response-Time", `${duration}`);
-
-      next();
-    });
+    req[symbolRequestTime] = process.hrtime();
 
     next();
   });
+
+  // Hook to be triggered just before response to be send
+  instance.addHook('onSend', (request, reply, payload, next) => {
+    const requestTime = request.req[symbolRequestTime];
+    // Calculate the duration, in nanoseconds …
+    const hrDuration = process.hrtime(requestTime);
+    // … convert it to milliseconds …
+    const duration = Math.round(hrDuration[0]*1e5 + hrDuration[1]/1e4) / 100;
+    // … add the header to the response
+    reply.header("X-Response-Time", `${duration}`);
+
+    next();
+  });
+
 
   next();
   // Not before 0.31 (onSend hook added to this version)
